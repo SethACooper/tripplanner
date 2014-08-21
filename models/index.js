cbeba292cfccacc560a0d5e091a5110f21117fa6
Zeke Nierenberg <hampzan09@gmail.com>
2014-08-21 17:11:14 -0400

    reviews 1 and 2

---
+++ models/index.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/tp_101");
+mongoose.connection.on('error',function(err) {
+  console.log(err);
+});
+
+var Schema = mongoose.Schema;
+
+var placeSchema = new Schema({
+  address: String,
+  city: String,
+  state: String,
+  phone: String,
+  location: [Number, Number]
+});
+
+var hotelSchema = new Schema({
+  name: String,
+  place: [placeSchema],
+  num_stars: Number,
+  amenities: String
+});
+
+var thingsToDoSchema = new Schema({
+  name: String,
+  place: [placeSchema],
+  age_range: String
+});
+
+var restaurantSchema = new Schema({
+  name: String,
+  place: [placeSchema],
+  cuisine: String,
+  price: Number
+});
+
+
+module.exports = {
+  Place: mongoose.model('Place',placeSchema),
+  Hotel: mongoose.model('Hotel',hotelSchema),
+  ThingsToDo: mongoose.model('ThingsToDo',thingsToDoSchema),
+  Restaurant: mongoose.model("Restaurant",restaurantSchema)
+};
